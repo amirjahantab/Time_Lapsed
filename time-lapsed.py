a5387ed324d55b2897151b284769c260fe893103
amirreza jahantab | امیررضا جهانتاب <111413014+amirjahantab@users.noreply.github.com>
2023-08-20 14:05:26 +0330

    Update time-lapsed.py

--- time-lapsed.py
+++ time-lapsed.py
@@ -1,17 +1,50 @@
+import tkinter as tk
 import time
 
-def time_convert(sec):
-  mins = sec // 60
-  sec = sec % 60
-  hours = mins // 60
-  mins = mins % 60
-  print("Time Lapsed = {0}:{1}:{2}".format(int(hours),int(mins),sec))
+class StopWatch:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Stopwatch")
 
-input("Press Enter to start")
-start_time = time.time()
+        self.running = False
+        self.elapsed_time = 0
 
-input("Press Enter to stop")
-end_time = time.time()
+        self.time_label = tk.Label(root, text="00:00:00", font=("Helvetica", 48))
+        self.time_label.pack()
 
-time_lapsed = end_time - start_time
-time_convert(time_lapsed)
+        self.start_button = tk.Button(root, text="Start", command=self.start)
+        self.start_button.pack()
+
+        self.stop_button = tk.Button(root, text="Stop", command=self.stop)
+        self.stop_button.pack()
+
+        self.reset_button = tk.Button(root, text="Reset", command=self.reset)
+        self.reset_button.pack()
+
+    def start(self):
+        if not self.running:
+            self.running = True
+            self.start_time = time.time()
+            self.update_time()
+
+    def stop(self):
+        if self.running:
+            self.running = False
+
+    def reset(self):
+        self.running = False
+        self.elapsed_time = 0
+        self.time_label.config(text="00:00:00")
+
+    def update_time(self):
+        if self.running:
+            elapsed_seconds = int(time.time() - self.start_time) + self.elapsed_time
+            hours = elapsed_seconds // 3600
+            minutes = (elapsed_seconds % 3600) // 60
+            seconds = elapsed_seconds % 60
+            self.time_label.config(text="{:02d}:{:02d}:{:02d}".format(hours, minutes, seconds))
+            self.root.after(1000, self.update_time)
+
+root = tk.Tk()
+stopwatch = StopWatch(root)
+root.mainloop()
